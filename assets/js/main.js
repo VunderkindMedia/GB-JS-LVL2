a25d541119a3e010caed4e1b8b92e48572e9979a
VunderkindMedia <idstrelov@okha65.ru>
2021-02-17 20:40:53 +1100

    LESSON 3
    Добавил асинхронные запросы к условному бэку.
    
    Добавил прелоадеры к списку товаров и кнопке добавления в корзину.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+
 class GoodItem {
   title;
   price;
@@ -10,10 +12,20 @@ class GoodItem {
 
   render() {
     return `<div class="product-item">
-            <div class="product-item__image"><img src="https://picsum.photos/200" alt="image"></div>
-            <h3 class="product-item__title">${this.title}</h3>
-            <p class="product-item__price">Price: ${this.price} USD</p>
-            <div class="product-item__cart-btn cart-button">Добавить в корзину</div>
+              <div class="product-item__image"><img src="https://picsum.photos/200" alt="image"></div>
+              <h3 class="product-item__title">${this.title}</h3>
+              <p class="product-item__price">Price: ${this.price} USD</p>
+              <div class="product-item__cart-btn cart-button">
+              <span>Добавить в корзину</span>
+                <div class="cssload-container hide">
+                  <ul class="cssload-flex-container">
+                    <li>
+                      <span class="cssload-loading"></span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+           </div>
           </div>`;
   }
 }
@@ -24,16 +36,20 @@ class GoodsList {
     this.sum = 0;
   }
   getList() {
-    const productList = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
-    this.items = productList.map(({title = 'Заголовок отсутствует', price = 0}) => {
-      this.sum += price;
-      return new GoodItem(title, price).render();
-    }).join('');
+
+    const url = `${API_URL}/catalogData.json`
+    return fetch(url, {
+      method: 'GET'
+    }).then(result => {
+      return result.json();
+    }).then(result => {
+      console.log(result);
+      this.items = result.map(({id_product, product_name = 'Заголовок отсутствует', price = 0}) => {
+        this.sum += price;
+        return new GoodItem(product_name, price).render();
+      }).join('');
+      this.render();
+    })
   }
 
   getAllPrice() {
@@ -57,16 +73,23 @@ class CartList extends GoodsList {
     this.cart_btn.querySelector('p').innerHTML = this.getItemsCount();
   }
 
-  addItem(title, price) {
-    this.items.push({title, price});
-    this.updateCountCartBtn();
-    this.log();
+  addItem(element, title, price) {
+    const url = `${API_URL}/addToBasket.json`
+    return fetch(url, {
+      method: 'GET'
+    }).then(result => {
+      return result.json();
+    }).then(result => {
+      this.items.push({title, price});
+      this.updateCountCartBtn();
+      return result
+    })
+
   }
 
   deleteItem(item) {
     this.items.splice(this.items.indexOf(item));
     this.updateCountCartBtn();
-    this.log();
   }
 
   getItemsCount() {
@@ -74,17 +97,11 @@ class CartList extends GoodsList {
 
     return this.count;
   }
-
-  log() {
-    console.log(this.items);
-  }
 }
 
 ///Список товаров
 
 const goodList = new GoodsList();
-goodList.getList();
-goodList.render();
 
 ///Добавление/удаление из корзины
 
